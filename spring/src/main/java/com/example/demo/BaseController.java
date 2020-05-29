6fda3693d9d8fe4f6ef2f09f059844adb65fe1ee
Pavel Ivaschenko <pawloiwanov@gmail.com>
2020-05-29 21:59:38 +0300

    [add]: spring boot

---
+++ spring/src/main/java/com/example/demo/BaseController.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class BaseController {
+
+    @GetMapping
+    public String getIndex() {
+        return "Hello world!";
+    }
+}
