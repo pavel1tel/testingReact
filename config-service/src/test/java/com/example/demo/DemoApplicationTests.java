677b6e637771a46a7d5f89e6609314938196447f
Pavel Ivaschenko <pawloiwanov@gmail.com>
2020-05-30 18:28:45 +0300

    [update]: it works))

--- config-service/src/test/java/com/example/demo/DemoApplicationTests.java
+++ config-service/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,12 +1,10 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DemoApplicationTests {
 
-    @Test
     void contextLoads() {
     }
 
