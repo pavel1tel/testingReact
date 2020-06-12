cc5700257ff21182b7db9acbef168076c1fe8242
n4mespace <zaxarchuk.d@gmail.com>
2020-06-13 01:01:37 +0300

    feat: routing init

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import Auth from "./Components/Auth";
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from "react-router-dom";
 
-const App = (props) => {
+const AppComponent = (props) => {
   return (
-    <div className="App">
-      <Auth />
+    <div className="AppComponent">
+        <Router>
+            <header>
+                <nav>
+                    <ul>
+                        <li><Link to='/'>Home</Link></li>
+                    </ul>
+                </nav>
+            </header>
+
+            <Switch>
+                <Route exact path='/'>
+                    Main page
+                </Route>
+                <Route exact path='/accounts' component={Auth}/>
+                { 
+                // <Route path='/home/user' component={UserHome}/>
+                // <Route path='/home/inspector' component={InspectorHome}/>
+                }
+            </Switch>
+        </Router>
     </div>
   );
 }
 
-export default App;
+export default AppComponent;
--- client/src/Components/Auth/index.js
+++ client/src/Components/Auth/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { LoginForm } from './Forms';
+import {
+  Switch,
+  Route,
+  useRouteMatch,
+} from "react-router-dom";
 
 
 const Auth = (state) => {
+    const { path, url } = useRouteMatch();
+
     return (
         <div className='Auth-root'>
-            <LoginForm />
+            <Switch>
+                <Route path={`${path}/login`} component={LoginForm}/>
+                {
+                // <Route {`${path}/registration`} component={Registration}/>
+                }
+            </Switch>
         </div>
     );
 }
